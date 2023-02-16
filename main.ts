d37f075010d2cf86138d1db26c7e427b3824713b
Blopvisje <122538086+Blopvisje@users.noreply.github.com>
2023-02-16 14:16:37 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,41 @@
+let kompasrichting = 0
+basic.forever(function () {
+    if (kompasrichting < 135 && kompasrichting > 45) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (kompasrichting < 225 && kompasrichting > 135) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (kompasrichting < 315 && kompasrichting > 225) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    kompasrichting = input.compassHeading()
+})
